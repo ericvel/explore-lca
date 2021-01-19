5cadd8f593658c432782d98cd9cedcd83d36f304
Eric Veliyulin <eric.vel@outlook.com>
2021-01-19 13:32:26 +0100

    Add sample db config file

---
+++ backend/dbconfig_sample.js
@@ -0,0 +1,10 @@
+
+const config = {
+    MYSQL_HOST: 'localhost',
+    MYSQL_USER: 'root',
+    MYSQL_PWD: 'YOUR PASSWORD',
+    MYSQL_DB: 'YOUR DB',
+    MYSQL_PORT: '3306',
+};
+
+module.exports = config;
